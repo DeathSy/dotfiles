a79f2eccc8f1c43d89378cdd10d0ce3a930536ce
ksotis <ks.srikittichai@gmail.com>
2021-11-02 20:24:42 +0700

    update pre installation scripts

--- init.sh
+++ init.sh
@@ -4,8 +4,11 @@ xcode-select --install
 # INSTALL homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+brew install go antigen
+
 # ADD zsh configuration
-ln -s -f ./.zshrc ~/.zshrc
+ln -s -f ./antigen /usr/local/share/antigen
+ln -s -f $(pwd)/.zshrc ~/.zshrc
 
 # ADD tmux configuration
 ln -s -f $(pwd)/tmux.conf ~/.tmux.conf
@@ -16,6 +19,6 @@ ln -s -f $(pwd)/nvim ~/.config/nvim
 
 # ADD iCloud configurations config to it's place
 
-ln -s -f "~/Library/Mobile Documents/com~apple~CloudDocs/workspace" ~/workspacek
+ln -s -f "~/Library/Mobile Documents/com~apple~CloudDocs/workspace" ~/workspace
 ln -s -f "~/Library/Mobile Documents/com~apple~CloudDocs/workspace/keypairs" ~/.ssh/keypairs
 ln -s -f "~/Library/Mobile Documents/com~apple~CloudDocs/workspace/infra/config" ~/.ssh/config
