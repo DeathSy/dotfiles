b21952cbf830dc4b2279c3b684cfa6666a3395b5
deathsy <ks.srikittichai@gmail.com>
2024-03-23 12:01:12 +0700

    add iterm2 in brew init

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ xcode-select --install
 # INSTALL pngm
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 
-brew install go antigen nvm gh git-flow git-lfs helm neovim ruby terraform tree wget kubernetes-cli act ngrok vagrant htop rustup-init oven-sh/bun/bun openjdk@11 aquasecurity/trivy/trivy derailed/k9s/k9 lazydocker tnk-studio/tools/lazykube pipx
+brew install iterm2 go antigen nvm gh git-flow git-lfs helm neovim ruby terraform tree wget kubernetes-cli act ngrok vagrant htop rustup-init oven-sh/bun/bun openjdk@11 aquasecurity/trivy/trivy derailed/k9s/k9 lazydocker tnk-studio/tools/lazykube pipx
 
 brew tap homebrew/cask-fonts adoptopenjdk/openjdk
 brew install --cask font-fira-code visual-studio-code runjs font-hack-nerd-font raycast adoptopenjdk8
