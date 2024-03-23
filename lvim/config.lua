ab90a073f3098d14e9cd3622e4106104e0d0706a
deathsy <ks.srikittichai@gmail.com>
2024-03-23 16:49:18 +0700

    update remap key

--- lvim/config.lua
+++ lvim/config.lua
@@ -2,3 +2,24 @@ reload("ks.options")
 reload("ks.treesitter")
 reload("ks.keymaps")
 reload("ks.mason")
+
+lvim.plugins = {
+  -- vim motion plugins --
+  { "tpope/vim-surround" },
+  { "mg979/vim-visual-multi" },
+  {
+    "ggandor/leap.nvim",
+    name = "leap",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
+
+  -- Themes --
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  }
+}
--- lvim/lua/ks/options.lua
+++ lvim/lua/ks/options.lua
@@ -1,3 +1,8 @@
+lvim.colorscheme = "tokyonight-night"
+lvim.transparent_window = true
+lvim.format_on_save = true
+
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
