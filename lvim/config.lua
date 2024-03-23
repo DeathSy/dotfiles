47456aa393dfb1f909ecebc70b7dfa0389734ca8
deathsy <ks.srikittichai@gmail.com>
2024-03-23 20:10:46 +0700

    uninstall copilot

--- lvim/config.lua
+++ lvim/config.lua
@@ -33,18 +33,6 @@ lvim.plugins = {
   },
 
   -- Productivity --
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    after = { "copilot.lua" },
-    config = function()
-      require("copilot_cmp").setup()
-    end,
-  },
 
   -- Themes --
   {
