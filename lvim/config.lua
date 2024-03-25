2dc3413a3a2a3d74d43e831df78c34832b89c057
deathsy <ks.srikittichai@gmail.com>
2024-03-25 19:53:05 +0700

    add git blame and sensible

--- lvim/config.lua
+++ lvim/config.lua
@@ -84,6 +84,14 @@ lvim.plugins = {
   -- Productivity --
   { "christoomey/vim-tmux-navigator" },
   {
+    "f-person/git-blame.nvim",
+    event = "BufRead",
+    config = function()
+      vim.cmd "highlight default link gitblame SpecialComment"
+      vim.g.gitblame_enabled = 1
+    end,
+  },
+  {
     "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
     config = function()
