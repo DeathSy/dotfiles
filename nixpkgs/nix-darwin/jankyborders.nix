a7982364df461419601022f16c87caaff0b96453
DeathSy <ks.srikittichai@gmail.com>
2025-03-22 18:43:03 +0700

    move jankyborder config

---
+++ nixpkgs/nix-darwin/jankyborders.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, lib, ... }:
+{
+  service.jankyborders = {
+    enable = true;
+    style = "round";
+    hidpi = false;
+    active_color = "0xc0e2e2e3";
+    inactive_color = "0xc02c2e34";
+    background_color = "0x302c2e34";
+    width = 6.0;
+  };
+}
